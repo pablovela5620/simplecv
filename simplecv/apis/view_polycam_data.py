b390bce435e11ba0fce8f7382d4fbba78e7e8f44
pablovela5620 <pablovela5620@gmail.com>
2025-01-19 18:51:30 -0600

    enhance polycam data logging with image resizing and blueprint creation

--- simplecv/apis/view_polycam_data.py
+++ simplecv/apis/view_polycam_data.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
 from pathlib import Path
 
+import cv2
 import numpy as np
 import open3d as o3d
 import rerun as rr
+import rerun.blueprint as rrb
 from jaxtyping import UInt8, UInt16
 from tqdm import tqdm
 
+from simplecv.camera_parameters import Intrinsics, rescale_intri
 from simplecv.data.polycam import (
     DepthConfidenceLevel,
     PolycamData,
@@ -22,12 +25,14 @@ class PolyViewConfig:
     polycam_zip_path: Path
     rr_config: RerunTyroConfig
     log_incremental_mesh: bool = True
+    rescale_factor: int = 4
     # logs the mesh incrementally, will take up more memory
 
 
 def log_polycam_data(
     parent_path: Path,
     polycam_data: PolycamData,
+    rescale_factor: int = 1,
 ) -> None:
     cam_path: Path = parent_path / "cam"
     pinhole_path: Path = cam_path / "pinhole"
@@ -36,10 +41,42 @@ def log_polycam_data(
     depth: UInt16[np.ndarray, "h w"] = polycam_data.depth_hw
     confidence: UInt8[np.ndarray, "h w"] = polycam_data.confidence_hw
 
+    # resize images to be half the size
+    target_height: int = rgb.shape[0] // rescale_factor
+    target_width: int = rgb.shape[1] // rescale_factor
+    rgb_resized = cv2.resize(rgb, (target_width, target_height))
+    depth_resized = cv2.resize(depth, (target_width, target_height))
+    confidence_resized = cv2.resize(confidence, (target_width, target_height))
+
+    # rescale intrinsics to match the image size
+    rescaled_intrinsics: Intrinsics = rescale_intri(
+        camera_intrinsics=polycam_data.pinhole_params.intrinsics,
+        target_height=target_height,
+        target_width=target_width,
+    )
+
+    polycam_data.pinhole_params.intrinsics = rescaled_intrinsics
+
     log_pinhole(camera=polycam_data.pinhole_params, cam_log_path=cam_path)
-    rr.log(f"{pinhole_path}/image", rr.Image(rgb))
-    rr.log(f"{pinhole_path}/confidence", rr.SegmentationImage(confidence))
-    rr.log(f"{pinhole_path}/gt_depth", rr.DepthImage(depth, meter=1000))
+    rr.log(f"{pinhole_path}/image", rr.Image(rgb_resized).compress(jpeg_quality=75))
+    rr.log(f"{pinhole_path}/confidence", rr.SegmentationImage(confidence_resized))
+    rr.log(f"{pinhole_path}/gt_depth", rr.DepthImage(depth_resized, meter=1000))
+
+
+def create_blueprint(parent_log_path: Path) -> rrb.Blueprint:
+    blueprint = rrb.Blueprint(
+        rrb.Horizontal(
+            rrb.Spatial3DView(),
+            rrb.Vertical(
+                rrb.Spatial2DView(origin=f"{parent_log_path}/cam/pinhole/image"),
+                rrb.Spatial2DView(origin=f"{parent_log_path}/cam/pinhole/gt_depth"),
+                rrb.Spatial2DView(origin=f"{parent_log_path}/cam/pinhole/confidence"),
+            ),
+            column_shares=[4, 1],
+        ),
+        collapse_panels=True,
+    )
+    return blueprint
 
 
 def view_polycam_data(config: PolyViewConfig) -> None:
@@ -50,7 +87,9 @@ def view_polycam_data(config: PolyViewConfig) -> None:
     depth_fuser = Open3DFuser(fusion_resolution=0.04, max_fusion_depth=3.0)
 
     parent_path: Path = Path("world")
-    rr.log(f"{parent_path}", rr.ViewCoordinates.RUB, timeless=True)
+    rr.log("/", rr.ViewCoordinates.RUB, timeless=True)
+    blueprint: rrb.Blueprint = create_blueprint(parent_log_path=parent_path)
+    rr.send_blueprint(blueprint)
 
     pbar = tqdm(polycam_dataset, total=len(polycam_dataset))
     polycam_data: PolycamData
@@ -72,8 +111,10 @@ def view_polycam_data(config: PolyViewConfig) -> None:
         log_polycam_data(
             parent_path=parent_path,
             polycam_data=polycam_data,
+            rescale_factor=config.rescale_factor,
         )
 
+        # log mesh on every frame, this will take up more memory and computation, but looks way cooler
         if config.log_incremental_mesh:
             gt_mesh: o3d.geometry.TriangleMesh = depth_fuser.get_mesh()
             gt_mesh.compute_vertex_normals()
