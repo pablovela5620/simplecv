69b9fb3e28f5a7aa8c229d54824cb04828b7b8f5
pablo vela <pablovela5620@gmail.com>
2025-06-30 15:24:26 -0500

    feat: Add log_exoego_batch function and refactor visualize_exo_ego for improved logging of exoego sequences

--- simplecv/data/exoego/base_exoego.py
+++ simplecv/data/exoego/base_exoego.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from collections.abc import Generator
 from dataclasses import dataclass
+from pathlib import Path
 
 from jaxtyping import Float
 from numpy import ndarray
@@ -34,6 +35,7 @@ class BaseExoEgoSequence(ABC):
         self.config: BaseExoEgoDatasetConfig = cfg
         self.ego_sequence: BaseEgoSequence | None = self._build_ego()
         self.exo_sequence: BaseExoSequence | None = self._build_exo()
+
         if self.config.load_labels:
             self._exoego_labels: ExoEgoLabels = self.load_labels()
 
@@ -61,6 +63,14 @@ class BaseExoEgoSequence(ABC):
     @abstractmethod
     def load_labels(self) -> ExoEgoLabels:
         """Load labels for the sequence, if applicable."""
+
+    # def project_xyz(self):
+    # xyz_hom: Float32[ndarray, "21 4"] = np.hstack((xyz, np.ones((21, 1)))).astype(np.float32)
+    # P_ego: Float32[ndarray, "3 4"] = current_ego_cam.projection_matrix.astype(np.float32)
+    # P_ego: Float32[ndarray, "1 3 4"] = rearrange(P_ego, "n m -> 1 n m")
+    # uvc_ego: Float32[ndarray, "1 21 3"] = projectN3(xyz_hom, P_ego).astype(np.float32)
+    # uv_ego: Float32[ndarray, "21 2"] = uvc_ego[0, :, :2]
+    # uv_ego[uv_ego == -1] = np.nan
 
     @property
     @abstractmethod
--- simplecv/data/exoego/exoego_config.py
+++ simplecv/data/exoego/exoego_config.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from pathlib import Path
 
 from simplecv.configs.base_config import InstantiateConfig
 
@@ -6,3 +7,4 @@ from simplecv.configs.base_config import InstantiateConfig
 @dataclass
 class BaseExoEgoDatasetConfig(InstantiateConfig):
     load_labels: bool = True
+    parent_log_path: Path = Path("world")
