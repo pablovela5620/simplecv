2af68878e3f3d5dd76f3acba5e6d178784a62c3a
pablo vela <pablovela5620@gmail.com>
2025-09-08 13:34:07 -0500

    feat(remux): Implement remuxing functionality with CLI support and H.264 handling.
    Makes it easy to call from other parts of the co.
    
    remove old debug video_stereo_rrd

--- simplecv/rerun_log_utils.py
+++ simplecv/rerun_log_utils.py
@@ -1,14 +1,19 @@
+import io
 import sys
 from dataclasses import dataclass, field
+from fractions import Fraction
 from pathlib import Path
 from typing import Any
 from uuid import UUID
 
+import av
 import numpy as np
 import pyarrow as pa
 import rerun as rr
 from jaxtyping import Float, Int, UInt8
 from numpy import ndarray
+from pyarrow import ChunkedArray
+from rerun_bindings import Recording, RecordingView
 
 from simplecv.camera_parameters import PinholeParameters
 
@@ -253,3 +258,66 @@ class Points3DWithConfidence(rr.ComponentColumn):
             list(self.points3d.as_component_batches())  # The components from Points3D
             + [self.confidences]  # Custom confidence data
         )
+
+
+def read_h264_samples_from_rrd(rrd_path: str, video_entity: str, timeline: str) -> tuple[ChunkedArray, ChunkedArray]:
+    """Load recording data and query video stream."""
+
+    recording: Recording = rr.dataframe.load_recording(rrd_path)
+    view: RecordingView = recording.view(index=timeline, contents=video_entity)
+
+    # Make sure this is H.264 encoded.
+    # For that we just read out the first codec value batch and check whether it's H.264.
+    codec = view.select(f"{video_entity}:VideoStream:codec")
+    first_codec_batch = codec.read_next_batch()
+    if first_codec_batch is None:
+        raise ValueError(f"There's no video stream codec specified at {video_entity} for timeline {timeline}.")
+    codec_value = first_codec_batch.column(0)[0][0].as_py()
+    if codec_value != rr.VideoCodec.H264.value:
+        raise ValueError(
+            f"Video stream codec is not H.264 at {video_entity} for timeline {timeline}. "
+            f"Got {hex(codec_value)}, but the value for H.264 is {hex(rr.VideoCodec.H264.value)}."
+        )
+    else:
+        print(f"Video stream codec is H.264 at {video_entity} for timeline {timeline}.")
+
+    # Get the video stream
+    timestamps_and_samples = view.select(timeline, f"{video_entity}:VideoStream:sample").read_all()
+    times = timestamps_and_samples[0]
+    samples = timestamps_and_samples[1]
+
+    print(f"Retrieved {len(samples)} video samples.")
+
+    return times, samples
+
+
+def mux_h264_to_mp4(times: ChunkedArray, samples: ChunkedArray, output_path: str) -> None:
+    """Mux H.264 Annex B samples to an mp4 file using PyAV."""
+    # See https://pyav.basswood-io.com/docs/stable/cookbook/basics.html#remuxing
+
+    # Flatten out sample list into a single byte buffer.
+    sample_bytes = samples.combine_chunks().flatten(recursive=True)
+    sample_bytes = io.BytesIO(sample_bytes.buffers()[1])
+
+    # Setup samples as input container.
+    input_container = av.open(sample_bytes, mode="r", format="h264")  # Input is AnnexB H.264 stream.
+    input_stream = input_container.streams.video[0]
+
+    # Setup output container.
+    output_container = av.open(output_path, mode="w")
+    output_stream = output_container.add_stream_from_template(input_stream)
+
+    # Timestamps are made relative to the first timestamp.
+    start_time = times.chunk(0)[0]
+    print(f"Offsetting timestamps with start time: {start_time}")
+
+    # Demux and mux packets.
+    for packet, time in zip(input_container.demux(input_stream), times, strict=False):
+        packet.time_base = Fraction(1, 1_000_000_000)  # Assuming duration timestamps in nanoseconds.
+        packet.pts = int(time.value - start_time.value)
+        packet.dts = packet.pts  # dts == pts since there's no B-frames.
+        packet.stream = output_stream
+        output_container.mux(packet)
+
+    input_container.close()
+    output_container.close()
